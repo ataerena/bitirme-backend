d64ebf380c1502ffd4ad71f464a4ec77ab3cbf75
ataerena <ataerena599@gmail.com>
2024-01-06 21:47:14 +0300

    Album Creating Added

---
+++ requests/createAlbum.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const fs = require('fs');
+const userData = require('../database/users.json');
+const router = express.Router();
+
+router.post('/createAlbum', (req, res) => {
+    const { username, albumName } = req.body;
+
+    if (!username || !albumName) {
+        return res.status(400).send('Username or album name not provided.');
+    }
+
+    const user = userData.find((user) => user.username === username);
+
+    if (!user) {
+        return res.status(404).send('User not found.');
+    }
+
+    // Check if the album already exists for the user
+    if (user.albums && user.albums.includes(albumName)) {
+        return res.status(400).send('Album already exists for the user.');
+    }
+
+    // Update the user's albums array
+    if (!user.albums) {
+        user.albums = []; // Initialize albums array if not present
+    }
+    user.albums.push(albumName);
+
+    // Rewrite the updated user data back to the JSON file
+    fs.writeFileSync('../database/users.json', JSON.stringify(userData, null, 2));
+
+    res.status(200).send('Album created successfully.');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { loginUser } = require('../requests/login');
-const { registerUser } = require('../requests/register');
-const fileUploadRouter = require('../requests/fileUpload');
-const getImagesRouter = require('../requests/getImages');
-const updateImageRouter = require('../requests/updateImage');
-const getAlbumsRouter = require('../requests/getAlbums');
-const reauthenticationRouter = require('../requests/reauthentication');
+const { loginUser } = require('./requests/login');
+const { registerUser } = require('./requests/register');
+const fileUploadRouter = require('./requests/fileUpload');
+const getImagesRouter = require('./requests/getImages');
+const updateImageRouter = require('./requests/updateImage');
+const getAlbumsRouter = require('./requests/getAlbums');
+const reauthenticationRouter = require('./requests/reauthentication');
+const createAlbumRouter = require('./requests/createAlbum');
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.post('/getimages', getImagesRouter);
 app.use('/update', updateImageRouter);
 app.use('/get', getAlbumsRouter);
 app.use('/auth', reauthenticationRouter);
+app.use('/create', createAlbumRouter);
 
 const PORT = 9000;
 app.listen(PORT, () => {
